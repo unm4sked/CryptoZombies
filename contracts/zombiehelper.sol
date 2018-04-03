5306bdc92a6e7c69df291611d9cae61aed84e0d9
unm4sked <lukasz.k.kalicki@gmail.com>
2018-04-03 19:15:52 +0200

    payable function

--- contracts/zombiehelper.sol
+++ contracts/zombiehelper.sol
@@ -4,6 +4,14 @@ import "./zombiefeeding.sol";
 
 contract ZombieHelper is ZombieFeeding {
 
+
+    uint levelUpFee = 0.001 ether;
+
+    function levelUp(uint _zombieId) external payable {
+        require(msg.value == levelUpFee);
+        zombies[_zombieId].level++;
+    }
+
     modifier aboveLevel(uint _level, uint _zombieId) {
         require(zombies[_zombieId].level >= _level);
         _;
